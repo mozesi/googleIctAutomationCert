a215cf643bce1aac9c9181032d044626331b3266
mozesi <msukwamose1@gmail.com>
2023-05-08 11:43:44 +0200

    functions miles to kms

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ def courseStart(name):
     print("You have just enrolled for this course " + str(name))
 
 courseStart("Moses Msukwa")
+
+#convert miles to kilometers
+
+def ConvertDistance(miles):
+    km = miles * 1.6
+    return km
+
+myTripMiles = 55
+myTripKm = ConvertDistance(myTripMiles)
+print(myTripKm)
